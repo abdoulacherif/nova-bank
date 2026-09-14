10fb5720bbc42993ead5fa101eac9ceaabac98ed
abdoulacherif <abdoula13cherif@gmail.com>
2026-09-14 11:54:24 +0100

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "KontakApp"
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":shared")
+include(":composeApp")
