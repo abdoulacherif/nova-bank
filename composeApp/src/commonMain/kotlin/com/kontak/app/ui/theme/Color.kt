8a0c63e2cb26cb5b29bcd78ae6af5fd7e4cf0d71
abdoulacherif <abdoula13cherif@gmail.com>
2026-09-14 12:05:06 +0100

    Create src/commonMain/kotlin/com/kontak/app/ui/theme/Color.kt

---
+++ composeApp/src/commonMain/kotlin/com/kontak/app/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.kontak.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Exactement les mêmes valeurs que --variables du site (public/*.html)
+val KontakGreen = Color(0xFF25D366)
+val KontakGreenDark = Color(0xFF128C4A)
+val KontakInk = Color(0xFF0E2A1B)
+val KontakOrange = Color(0xFFFF8A3D)
+val KontakGold = Color(0xFFE8A33D)
+val KontakBgSoft = Color(0xFFF3FBF5)
+val KontakBg = Color(0xFFFFFFFF)
+val KontakErr = Color(0xFFD9534F)
+val KontakMuted = Color(0x8C0E2A1B) // ink à 55% d'opacité, comme rgba(14,42,27,0.55)
+val KontakLine = Color(0x1A0E2A1B) // ink à 10% d'opacité, comme rgba(14,42,27,0.10)
