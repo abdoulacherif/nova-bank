b64fc8885f68fdd674cc777b67f8d417c1a7acb9
abdoulacherif <abdoula13cherif@gmail.com>
2026-09-15 07:01:18 +0100

    Delete KontakHttpClient.ios.kt

--- shared/src/commonMain/kotlin/com/kontak/app/network/KontakHttpClient.ios.kt
+++
@@ -1,6 +0,0 @@
-package com.kontak.app.network
-
-import io.ktor.client.engine.HttpClientEngineFactory
-import io.ktor.client.engine.darwin.Darwin
-
-actual val kontakHttpEngine: HttpClientEngineFactory<*> = Darwin
