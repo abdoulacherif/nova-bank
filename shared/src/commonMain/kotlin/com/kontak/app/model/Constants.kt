d935b2584e1af8b53a6f61b9ad62764eac10eb6a
abdoulacherif <abdoula13cherif@gmail.com>
2026-09-15 22:05:51 +0100

    Create Constants.kt

---
+++ shared/src/commonMain/kotlin/com/kontak/app/model/Constants.kt
@@ -0,0 +1,12 @@
+package com.kontak.app.model
+
+val SECTORS = listOf(
+    "Commerce",
+    "Mode & beauté",
+    "Restauration",
+    "Services",
+    "Tech & digital",
+    "Immobilier",
+    "Éducation",
+    "Autre",
+)
