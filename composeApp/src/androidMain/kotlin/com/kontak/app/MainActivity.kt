83b41197930ffbf8ef057f104fe2a92b133cf79b
abdoulacherif <abdoula13cherif@gmail.com>
2026-09-17 08:25:40 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/kontak/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/kontak/app/MainActivity.kt
@@ -3,10 +3,12 @@ package com.kontak.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.kontak.app.platform.AndroidAppContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidAppContext.context = applicationContext
         setContent {
             App()
         }
